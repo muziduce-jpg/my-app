10da77bd808c2a32d536d01cc1c0029946f34ba1
Muzi Duce <muziduce@gmail.com>
2026-05-03 12:42:19 +0200

    Updated UI

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,12 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+    <div style={{{ textAlign: "center, marginTop: "50px" }}>
+      <h1>Muzi's Project Generator</h1>
+      <p>This is my first deployed React App.</p>
+      <button>Generate Idea</button>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
+    )
         <button
           type="button"
           className="counter"
